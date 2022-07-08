7a9c7fe155525179b2844f05ab360360081f5009
unknown <mauricio.pino.log@gmail.com>
2022-07-08 18:24:23 -0400

    footer agregado

--- js/main.js
+++ js/main.js
@@ -146,14 +146,14 @@ $(document).ready(function () {
 		arrows: true,
 		autoplayspeed: 2000,
 		infiniite: true,
-		autoplay: true,
+		/*autoplay: true,*/
 	
 		responsive: [
 	{
 	  breakpoint: 1024,
 	  settings: {
 		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToScroll: 1,
 		infinite: true,
 		/*dots: true*/
 	  }
@@ -162,7 +162,7 @@ $(document).ready(function () {
 	  breakpoint: 630,
 	  settings: {
 		slidesToShow: 2,
-		slidesToScroll: 2
+		slidesToScroll: 1
 	  }
 	},
 	{
